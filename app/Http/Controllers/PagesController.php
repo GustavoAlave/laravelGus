3bed416b666c8389742c8aac9776cf3214c074d3
Daniel Mamani Chino <mjdaniel.2323@gmail.com>
2023-12-07 10:09:13 -0500

    v5 Actualizacion

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -9,6 +9,35 @@ class PagesController extends Controller
     public function fnIndex () {
         return view('welcome');
     }
+    public function fnRegistrar(Request $request){
+
+        //return $request->all();         //Prueba de "token" y datos recibidos
+
+        $request ->validate([
+            'codEst' => 'required',
+            'nomEst' => 'required',
+            'apeEst' => 'required',
+            'fnaEst' => 'required',
+            'turnMat' => 'required',
+            'semMat' => 'required',
+            'estMat' => 'required'
+        ]);
+
+        $nuevoEstudiante = new Estudiante;
+        $nuevoEstudiante->codEst = $request->codEst;
+        $nuevoEstudiante->nomEst = $request->nomEst;
+        $nuevoEstudiante->apeEst = $request->apeEst;
+        $nuevoEstudiante->fnaEst = $request->fnaEst;
+        $nuevoEstudiante->turnMat = $request->turnMat;
+        $nuevoEstudiante->semMat = $request->semMat;
+        $nuevoEstudiante->estMat = $request->estMat;
+        
+        $nuevoEstudiante->save();
+        
+        //$xAlumnos = Estudiante1::all();                      //Datos de BD
+        //return view('pagLista', compact('xAlumnos'));        //Pasar a pagLista
+        return back()->with('msj','Se registro con éxito...'); //Regresar con msj
+    }
     public function fnEstDetalle($id){
         $xDetAlumnos = Estudiante::findOrFail($id);
         return view('Estudiante.pagDetalle', compact('xDetAlumnos'));
--- resources/views/pagLista.blade.php
+++ resources/views/pagLista.blade.php
@@ -1,13 +1,87 @@
 @extends('pagPlantilla')
 
 @section('titulo')
-    <h1 classs="display-4">Pagina Lista</h1>
+    <h1 class="display-4">PAGINA DE LISTA </h1>
 @endsection
 
 @section('seccion')
-    <h3>Lista</h3>
+    @if(session('msj'))
+        <div class="alert alert-success alert-dismissible fade show" role="alert">
+            {{ session('msj')}}
+        </div>
+    @endif
+
+    <form action="{{ route('Estudiante.xRegistrar')}}" method="post" class="d-grid gap-2">
+        @csrf
+
+        @error('codEst')
+            <div class="alert alert-danger">
+                Codigo Requerido
+            </div>
+        @enderror
+
+        @error('nomEst')
+            <div class="alert alert-danger">
+                Nombre Requerido
+            </div>
+        @enderror
+
+        @error('apeEst')
+            <div class="alert alert-danger">
+                Apellido Requerido
+            </div>
+        @enderror
+
+        @error('fnaEst')
+            <div class="alert alert-danger">
+                Fecha de nacimiento requerido
+            </div>
+        @enderror
+        @error('turnMat')
+            <div class="alert alert-danger">
+                Turno Requerido
+            </div>
+        @enderror
+        @error('semMat')
+            <div class="alert alert-danger">
+                Semestre requerido
+            </div>
+        @enderror
+        @error('estMat')
+            <div class="alert alert-danger">
+                estado requerido
+            </div>
+        @enderror
+
+        <input type="text" name="codEst" placeholder="Código" value="{{ old('codEst')}}" class="form-control mb-2">
+        <input type="text" name="nomEst" placeholder="Nombres" value="{{ old('nomEst')}}" class="form-control mb-2">
+        <input type="text" name="apeEst" placeholder="Apellidos" value="{{ old('apeEst')}}" class="form-control mb-2">
+        <input type="date" name="fnaEst" placeholder="Fecha de nacimiento" value="{{ old('fnaEst')}}" class="form-control mb-2">
+        <select name="turnMat" class="form-control mb-2">
+            <option value="">Seleccione...</option>
+            <option value="1">Turno Día</option>
+            <option value="2">Turno Noche</option>
+            <option value="3">Turno Tarde</option>
+        </select>
+        <select name="semMat" class="form-control mb-2">
+            <option value="">Seleccione...</option>
+            @for($i=1; $i < 7; $i++)
+                <option value="{{$i}}">Semestre {{$i}}</option>
+            @endfor
+        </select>
+        <select name="estMat" class="form-control mb-2">
+            <option value="">Seleccione...</option>
+            <option value="0">Inactivo</option>
+            <option value="1">Activo</option>
+        </select>
+        <button class="btn btn-success" type="submit">Agregar</button>
+    </form>
+
+    <br/>
+
+    <div class="btn btn-dark fs-3 fw-bold d-grid"> Lista de siguimiento</div>
     <table class="table">
-        <thead class="table-dark">
+        <thead>
             <tr>
                 <th scope="col">Id</th>
                 <th scope="col">Codigo</th>
@@ -16,18 +90,22 @@
             </tr>
         </thead>
         <tbody>
+    
+
             @foreach($xAlumnos as $item)
             <tr>
                 <th scope="row">{{ $item->id}}</th>
-                <td>{{ $item->codEst }}</td>
+                <td>{{ $item->codEst}}</td>
                 <td>
-                    <a href="{{ route('Estudiante.xDetalle', $item->id ) }}">
-                        {{ $item->apeEst }}, {{ $item->nomEst }}
+                    <a href="{{ route('Estudiante.xDetalle', $item->id) }}"> 
+                        {{ $item -> apeEst }} - {{ $item -> nomEst }} 
                     </a>
+
                 </td>
-                <td>@mdo</td>
+
             </tr>
             @endforeach
         </tbody>
     </table>
+
 @endsection
